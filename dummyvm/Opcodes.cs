1f26d53b5fcbfd6a3c2c390c410853f2380b40d7
Brian Angulo <brianangulo96@gmail.com>
2021-11-14 16:25:26 -0600

    doc: commenting on opcodes

--- dummyvm/Opcodes.cs
+++ dummyvm/Opcodes.cs
@@ -3,11 +3,11 @@ namespace dummyvm
 {
     public enum Opcodes
     {
-         HALT =  0x00,
-         PUSH =  0x01,
-         POP =   0x02,
-         PRINT = 0x03,
-         ADD =   0x04,
-         SUB =   0x05,
+         PUSH =  0x01,  // stops program execution. For simplicity this is also the default value for when program memory is empty. Every program should terminate with a halt instruction
+         HALT =  0x00,  // pushes the integer value on the instruction to the top of the stack
+         POP =   0x02,  // pops value at the top of the stack
+         PRINT = 0x03,  // this one for simplicity's sake it just prints value to the screen
+         ADD =   0x04,  // fetches and pops the top 2 values of the stack executes a addition then pushes the result to the top of the stack
+         SUB =   0x05,  // fetches and pops the top 2 values of the stack executes a substraction then pushes the result to the top of the stack
     }
 }
